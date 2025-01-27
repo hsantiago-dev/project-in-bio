fedee8b2009ecbe2baf0cec2ba546c1c953f0746
hsantiago-dev <henrick.bueno@hotmail.com>
2025-01-26 22:47:46 -0300

    feat: landing page header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
+import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Hero />
     </div>
-    // <Header />
-    // <Hero />
     // <VideoExplanation />
     // <Princing />
     // <FAQ />
-
   );
 }
